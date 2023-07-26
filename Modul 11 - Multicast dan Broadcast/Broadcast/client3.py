56e1efcd03f266ed59546db299791b320c36fe1e
whisnumurtyga <whisnumurty.galih.21@student.if.ittelkom-sby.ac.id>
2023-07-26 16:17:12 +0700

    kkk

--- Modul 11 - Multicast dan Broadcast/Broadcast/client3.py
+++ Modul 11 - Multicast dan Broadcast/Broadcast/client3.py
@@ -1,1 +1,31 @@
-git
+import socket
+import threading
+
+def receive_message():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            print(f'{message}')
+        except ConnectionResetError:
+            print('Server disconnected.')
+            break
+
+def send_message():
+    while True:
+        message = input('')
+        client_socket.send(message.encode('utf-8'))
+        if message == 'exit':
+            client_socket.close()
+            break
+
+host = '127.0.0.1'
+port = 8000
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((host, port))
+
+receive_thread = threading.Thread(target=receive_message)
+send_thread = threading.Thread(target=send_message)
+
+receive_thread.start()
+send_thread.start()
